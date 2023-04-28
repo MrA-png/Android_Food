c2462eaa9c250dd41cdc8f545725c9278bb92a50
Azhrul Azim Ripai <102673224+MrA-png@users.noreply.github.com>
2023-04-28 14:31:06 +0700

    first commit

---
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseUser firebaseUser;
+    private TextView textNamalengkap;
+    private Button btnLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textNamalengkap = findViewById(R.id.namalengkap);
+        btnLogout = findViewById(R.id.btn_logout);
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+
+        if(firebaseUser!=null){
+            textNamalengkap.setText(firebaseUser.getDisplayName());
+        }else{
+            textNamalengkap.setText("Login Gagal!!");
+        }
+
+        btnLogout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            finish();
+        });
+    }
+}
